6aa570ea8fc47424081cceb5cf8e24616d4ea7c0
aaronekdahl <aaronekahl@me.com>
2017-05-16 18:36:48 -0700

    pseudocode-in-work

---
+++ script.js
@@ -0,0 +1,17 @@
+var userArr = 
+
+/* This function creates a table row (tr) for each object in the array, with a cell (td) 
+for each property to be displayed. Append each of these rows to the table body.*/
+
+
+
+
+/*This handler is for the new-item button. It should hide the table and show the form. 
+It should set a handler for the form's submit button.*/
+
+
+
+
+/* This handler is for the submit button that creates an object using the form data and 
+adds that object to the array. Then it should call the first function you wrote to 
+recreate the table with the updated database and show that table, hiding the form.*/
